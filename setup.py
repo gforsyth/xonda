a93d393b82799753e13ff3a8e1fb9d3edf6c6a78
Gil Forsyth <gilforsyth@gmail.com>
2016-06-27 18:13:52 -0400

    tweak setup file

--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ name = "xonda"
 
 setup(
     name = name,
-    version = "0.1.0",
-    description = "A tool to easily switch between conda environments",
+    version = "0.1",
+    description = "A xonsh wrapper around conda",
     packages=['xontrib'],
     package_dir={'xontrib': 'xontrib'},
-    package_data={'xontrib': ['*.xsh']},
+    package_data={'xontrib': ['*.xsh', '*.py']},
+    author="Gil Forsyth",
+    author_email="gilforsyth@gmail.com",
     include_package_data=True,
 )
