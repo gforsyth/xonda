6a6b1b174f29b5820de702ce2a63ba3893c52450
Gil Forsyth <gilforsyth@gmail.com>
2016-06-28 12:15:26 -0400

    i hate python packaging so much

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
 from setuptools import setup
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), 'r') as f:
-    long_description = f.read()
 
 name = "xonda"
 
 setup(
     name=name,
-    version="0.1.1",
+    version="0.1.2",
     description="A xonsh wrapper around conda",
-    long_description=long_description,
     packages=['xontrib'],
     package_dir={'xontrib': 'xontrib'},
     package_data={'xontrib': ['*.xsh']},
