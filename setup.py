1b679d8ec6dfc6fb883631d6c8deee7b9a848c67
Gil Forsyth <gilforsyth@gmail.com>
2019-02-23 08:31:13 -0500

    bumped version to 0.9.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ name = 'xonda'
 
 setup(
     name=name,
-    version='0.8.0',
+    version='0.9.0',
     license='MIT',
     url='https://github.com/gforsyth/xonda',
     description='A xonsh wrapper around conda',
